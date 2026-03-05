44d65173df405dc13aeef4f82a2af130194552f1
Hassan Tanveer <hassant1035@gmail.com>
2026-03-05 23:44:20 +0500

    MyHashMap

--- Leetcode-easy/easy.py
+++ Leetcode-easy/easy.py
@@ -607,7 +607,38 @@ class MyHashSet:
 
     def contains(self, key: int) -> bool:
         return key in self.sss
+class MyHashMap:
 
+    def __init__(self):
+        self.keys:List[int]=[]
+        self.values:List[int]=[]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.keys:
+            index=self.keys.index(key)
+            self.values[index]=value
+        else:
+            self.keys.append(key)
+            self.values.append(value)
+
+    def get(self, key: int) -> int:
+        if key in self.keys:
+            index=self.keys.index(key)
+            return self.values[index]
+        else:
+            return -1
+    def remove(self, key: int) -> None:
+        if key in self.keys:
+            index=self.keys.index(key)
+            del self.keys[index]
+            del self.values[index]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
