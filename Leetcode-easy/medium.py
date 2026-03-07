6f146f94e33f01dba20140ca9245247b8b059e7a
Hassan Tanveer <hassant1035@gmail.com>
2026-03-07 22:26:02 +0500

    maxDepth

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -258,3 +258,10 @@ def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
             
         dfs(root,low,high)
         return ans[0]
+
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+    l=self.maxDepth(root.left)
+    r=self.maxDepth(root.right)
+    return 1+max(l,r)
