0e337e5495bbe51e5ac9ed2d7eab9299f1feaaba
Hassan Tanveer <hassant1035@gmail.com>
2026-03-14 23:22:46 +0500

    permute

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -529,3 +529,21 @@ class Solution:
         node.left=self.sortedArrayToBST(nums[:mid])
         node.right=self.sortedArrayToBST(nums[mid+1:])
         return node    
+
+def permute(self, nums: List[int]) -> List[List[int]]:
+        res:List[int]=[]
+        used:List[bool]=[False]*len(nums)
+        def backtrack(current:List[int]):
+            if len(current)==len(nums):
+                res.append(current)
+            
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i]=True
+                    newCurrent:List[int]=copy.copy(current)
+                    newCurrent.append(nums[i])
+                    backtrack(newCurrent)
+                    used[i]=False
+            
+        backtrack([])
+        return res
