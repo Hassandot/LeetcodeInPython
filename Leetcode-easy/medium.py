96f923cb403d5962ff807a799927fa4b83736725
Hassan Tanveer <hassant1035@gmail.com>
2026-03-19 02:48:02 +0500

    levelOrder

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -816,3 +816,20 @@ def majorityElement(self, nums: List[int]) -> List[int]:
             if value>k:
                 res.append(key)
         return res
+def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root :
+            return []
+        res=[]
+        queue=deque([root])
+        while queue:
+            level=[]
+            level_size=len(queue)
+            for i in range(level_size):
+                node=queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
