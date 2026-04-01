60a150b6ded5995f2856070e17f1d7dee8194fe1
Hassan Tanveer <hassant1035@gmail.com>
2026-04-01 20:01:22 +0500

    productExceptSelf

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -861,3 +861,14 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
                 break
             res.append(x[0])
         return res  
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix=[1]*(len(nums)+1)
+        suffix=[1]*(len(nums)+1)
+        for i in range(len(nums)):
+            prefix[i+1]=prefix[i]*nums[i]
+        for i in range(len(nums)-1,-1,-1):
+            suffix[i]=suffix[i+1]*nums[i]
+        res=[]
+        for i in range(len(nums)):
+            res.append(prefix[i]*suffix[i+1])
+        return res
