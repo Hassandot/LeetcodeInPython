80c9ecaa952a2c16344f0c5af382241a2139faf2
Hassan Tanveer <hassant1035@gmail.com>
2026-04-04 01:58:06 +0500

    dailyTemperatures

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -921,3 +921,17 @@ def evalRPN(self, tokens: List[str]) -> int:
             else:
                 stack.append(int(tokens[i]))
         return stack.pop()
+
+def dailyTemperatures(self, t: List[int]) -> List[int]:
+        res=[0]*len(t)
+        stack=[]
+        for i in range(len(t)):
+            if not stack:
+                stack.append(i)
+                continue
+            else:
+                while stack and t[stack[-1]]<t[i]:
+                    res[stack[-1]]=i-stack[-1]
+                    stack.pop()
+                stack.append(i)
+        return res
