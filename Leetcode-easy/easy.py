553389bc058b2f0c8dd02fd5167fd62e0dd97816
Hassan Tanveer <hassant1035@gmail.com>
2026-04-13 03:04:13 +0500

    countOperations

--- Leetcode-easy/easy.py
+++ Leetcode-easy/easy.py
@@ -1391,3 +1391,13 @@ def totalMoney(self, n: int) -> int:
             money+=(7*(2*i+6))//2
         money+=(remainingDays*(2*(weeks+1)+(remainingDays-1)))//2
         return money
+def countOperations(self, num1: int, num2: int) -> int:
+        count=0
+        while num1!=0 and num2!=0:
+            if num1>=num2:
+                num1-=num2
+            else:
+                num2-=num1
+            count+=1
+        return count
+        
