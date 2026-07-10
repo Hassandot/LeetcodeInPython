504e02dd5838682e25b12bfd5f9de6b423e57510
Hassan <hassant1035@gmail.com>
2026-07-11 00:21:09 +0500

    trafficSignal

--- Leetcode-easy/easy.py
+++ Leetcode-easy/easy.py
@@ -1464,3 +1464,13 @@ class Solution:
                 i+=1
                 j-=1
         return len(mpp)
+    
+class Solution:
+    def trafficSignal(self, timer: int) -> str:
+        if timer==0:
+            return "Green"
+        if timer==30:
+            return "Orange"
+        if timer>30 and timer<=90:
+            return "Red"
+        return "Invalid"
