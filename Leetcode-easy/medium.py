37fa5e80486f41f8dd31578ca55049b6c77fcc3d
Hassan <hassant1035@gmail.com>
2026-07-18 00:26:25 +0500

    sumRegion

--- Leetcode-easy/medium.py
+++ Leetcode-easy/medium.py
@@ -1264,3 +1264,26 @@ class Solution:
             numberOfOnes+=1
             rem=(rem*10+1)%k
         return numberOfOnes
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        m=len(matrix)
+        n=len(matrix[0])
+        self.mat:List[List[int]]=[[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                self.mat[i][j]=matrix[i][j]
+        self.prefixMat:List[List[int]]=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                self.prefixMat[i][j]=self.mat[i-1][j-1]+self.prefixMat[i-1][j]+self.prefixMat[i][j-1]-self.prefixMat[i-1][j-1]
+                
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        query:int=self.prefixMat[row2+1][col2+1] - self.prefixMat[row2+1][col1] - self.prefixMat[row1][col2+1] + self.prefixMat[row1][col1]
+        return query        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
